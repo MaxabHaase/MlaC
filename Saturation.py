ce23bcc3d0efd81eef80a37651d1988b994ffb43
MaxabHaase <maxabhaase@gmail.com>
2018-04-10 14:33:05 -0400

    saves to csv

--- Saturation.py
+++ Saturation.py
@@ -1,4 +1,5 @@
 from Bio.Seq import Seq
+import csv
 
 # my sequence (MlaC +/- 100bp)
 gene_seq = "TGACAATAAGAATAGTGGCGATGCGCCAGCTGCTGCGCCAGGTAATAATGAAACCACTGAACCTGTGGGTACAACGAAATAATTTCAGGAGAACCGACGCATGT" \
@@ -16,6 +17,7 @@ stop_codon_position = 733
 
 # codon number is the length of the gene (from start to stop), divided by three, excluding the stop.
 codon_number = (733 - 100)/3
+primers_ = {}
 
 # function for going codon by codon, getting sequence -/+ 15bp upstream codon, mergeing with NNS and printing the seq
 #for loop to go codon by codon (start to stop)
@@ -28,6 +30,8 @@ codon_number = (733 - 100)/3
     #calculate the Tm (http://www.biophp.org/minitools/melting_temperature/demo.php?formula=basic)
     #variable used for counting if Tm needs to be adjusted
     #if Tm is  less than 60 enter the while loop. which adds 1 base at each end to increse, until >60
+
+
 def Codon_by_codon(start, end, codon_number):
     for x_ in range(0, int(codon_number)):
         start_ = start + 3*x_
@@ -59,8 +63,14 @@ def Codon_by_codon(start, end, codon_number):
         P_rc = P.reverse_complement()
         y, z, w, x, = P.count('G'), P.count('C'), P.count('A'), P.count('T')
         Tm_rc = 64.9 + 41 * (1 + y + z - 16.4) / (3 + w + x + y + z)
-        print(P, "\t",  round(Tm), "\t", P_rc, "\t", round(Tm_rc))
-    pass
+        forward = 'F' + str(x_+1)
+        reverse = 'R' + str(x_+1)
+        primers_[forward] = str(P), round(Tm), len(P)
+        primers_[reverse] = str(P_rc), round(Tm_rc), len(P_rc)
+        # print(P, "\t",  round(Tm), "\t", P_rc, "\t", round(Tm_rc))
 
 
 Codon_by_codon(start_codon_position,stop_codon_position, codon_number)
+w = csv.writer(open("Primers.csv", "w"))
+for key, val in primers_.items():
+    w.writerow([key, val[0], val[1], val[2]])
